a90090f8dcc38ee945bc20b2e3c6156bcf5ce39c
Ivo Batkovic <ivobatkovic@gmail.com>
2019-12-01 11:07:46 -0800

    updated minor bugg for part 1 and added solution for part 2

--- 2019/day-1/python/fuel_counter.py
+++ 2019/day-1/python/fuel_counter.py
@@ -1,28 +1,43 @@
-import numpy as  np
-
 def fuel_counter(masses):
   """ Function that computes the fuel needed for the input masses.
   Expected input is required to either be a list/tuple or scalar"""
   if isinstance(masses,(tuple,list)):
-    return int(sum(round(mass/3)-2 for mass in masses))
-  return int(round(masses/3)-2)
+    return sum(fuel_counter(mass) for mass in masses)
+  return max(int(round(masses/3)-2),0)
+
+def fuel_needed_total(mass):
+  """ Function that computes the fuel needed for the mass and its own fuel.
+  Expected input is a """
+  required_fuel = fuel_counter(mass)
+  if required_fuel > 0:
+    return required_fuel + fuel_needed_total(required_fuel)
+  return 0
+  
+def fuel_counter_extended(masses):
+  """ Function that computes the fuel needed for the input masses and the extra fuel needed for the new fuel itself.
+  Expected input is required to either be a list/tuple or scalar"""
+  if isinstance(masses,(tuple,list)):
+    return sum(fuel_counter_extended(mass) for mass in masses)
+  return fuel_needed_total(masses)
+
+  fuel = fuel_counter(masses)
+  extra_fuel = fuel_needed_for_fuel(fuel)
+  return fuel+extra_fuel
 
 def main():
   # Open data file and read through all lines
-  filepath = "../data.txt"
-
+  file_location = "../data.txt"
   try:
-    with open(filepath) as f:
+    with open(file_location) as f:
       masses = f.readlines()
     # Remove whitespace characters at end of each line
     masses = [int(x.strip()) for x in masses] 
-    
-    print(fuel_counter(masses))
-  except:
-    print("Cannot file at: " + filepath)
+    f.close()
+    # Print solutions
+    print("Fuel needed for part one: " + str(fuel_counter(masses)))
+    print("Fuel needed for part two: " + str(fuel_counter_extended(masses)))
+  except IOError:
+    print("Cannot find file at: " + file_location)
   
 if __name__ == "__main__":
   main()
-
-
-
