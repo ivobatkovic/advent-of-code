165d1ec5d2586851f3cbaa5e786ebe669c256903
Ivo Batkovic <ivobatkovic@gmail.com>
2020-12-03 07:47:08 +0100

    2020 day3 solution

---
+++ 2020/day3/python/day3.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+from os.path import dirname, realpath, join
+import time, pytest
+
+def transform_input(input_):
+  # custom transform for the day
+  return input_
+
+
+def read_input(file_name = '../data/input.txt'):
+  dir_path = dirname(realpath(__file__))
+  with open(file_name, 'r') as f:
+    input_ = [transform_input(x.strip()) for x in f.readlines()]
+
+  return input_
+
+def solve_part1(input_):
+  h, w = len(input_), len(input_[0])
+
+  s = 0
+  for y in range(1,h):
+    x = 3*y
+    s += 1 if input_[y][x%w] == '#' else 0
+  return s
+    
+def solve_part2(input_):
+  h, w = len(input_), len(input_[0])
+
+  x_diff = [1, 3, 5, 7] + [0.5]
+  y_diff = [1] * 4      + [2]
+  
+  prodTrees = 1
+  for dx, dy in zip(x_diff,y_diff):
+    nTrees = 0
+    for y in range(0,h-1,dy):
+      nTrees += 1 if input_[y+dy][int((y+dy) * dx) % w] == '#' else 0
+    prodTrees *= nTrees
+
+  return prodTrees
+
+def main():
+  
+  input_ = read_input('../data/input.txt')
+  
+  t0 = time.time()
+  part1 = solve_part1(input_)
+  time_part1 = round((time.time()-t0)*1e3)
+  print(f'Solution to part one: {part1} (time taken {time_part1}[ms])')
+
+  t0 = time.time()
+  part2 = solve_part2(input_)
+  time_part2 = round((time.time()-t0)*1e3)
+  print(f'Solution to part one: {part2} (time taken {time_part2}[ms])')
+
+
+if __name__ == '__main__':
+  main()
+
+@pytest.mark.parametrize("input1,output1", [
+  ('../data/test_input0.txt', 7)
+])
+
+def test1(input1,output1):
+  assert solve_part1(read_input(input1)) == output1
+
+@pytest.mark.parametrize("input2,output2", [
+  ('../data/test_input0.txt', 336)
+])
+
+def test2(input2,output2):
+  assert solve_part2(read_input(input2)) == output2
