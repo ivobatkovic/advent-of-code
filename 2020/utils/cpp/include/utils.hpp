b28cc1ac749cf6501db36ac2fe80e7612b683ded
Ivo Batkovic <ivobatkovic@gmail.com>
2020-12-08 14:37:57 +0100

    updated python io headers

--- 2020/utils/cpp/include/utils.hpp
+++ 2020/utils/cpp/include/utils.hpp
@@ -11,6 +11,26 @@
 
 namespace utils {
 
+std::vector<std::string> read_file(std::string file_name);
+
+template <typename T>
+void push_transform(std::string str, std::vector<T> &split) {
+  // If the template is a string, we need to preserve the whitespaces
+  if (std::is_same<T, std::string>::value) {
+    T value;
+    for (auto &x_ : str) {
+      value += x_;
+    }
+    split.push_back(value);
+    // Otherwise, convert to a value
+  } else {
+    std::istringstream iss(str);
+    T value;
+    iss >> value;
+    split.push_back(value);
+  }
+}
+
 /**
  * @brief Split a string with delimiter
  *
@@ -19,34 +39,25 @@ namespace utils {
  * @return std::vector<std::string>
  */
 template <typename T>
-void splitString(std::string str, char delim, std::vector<T> &split) {
+void split_string(std::string str, std::string delim, std::vector<T> &split) {
   size_t start;
   size_t end = 0;
 
   while ((start = str.find_first_not_of(delim, end)) != std::string::npos) {
     end = str.find(delim, start);
-    // If the template is a string, we need to preserve the whitespaces
-    if (std::is_same<T, std::string>::value) {
-      T value;
-      for (auto &x_ : str.substr(start, end - start)) {
-        value += x_;
-      }
-      split.push_back(value);
-      // Otherwise, convert to a value
-    } else {
-      std::istringstream iss(str.substr(start, end - start));
-      T value;
-      iss >> value;
-      split.push_back(value);
-    }
+    push_transform(str.substr(start, end - start), split);
   }
+}
+template <typename T>
+void split_string(std::string str, char delim, std::vector<T> &split) {
+  split_string(str, std::string(1, delim), split);
 }
 
 /**
  * @brief Read lines of a file
  *
  */
-std::vector<std::string> readFile(std::string file_name) {
+std::vector<std::string> read_file(std::string file_name) {
   std::ifstream file(file_name);
   if (!file) {
     std::cerr << "Cannot open the File : " << file_name << std::endl;
