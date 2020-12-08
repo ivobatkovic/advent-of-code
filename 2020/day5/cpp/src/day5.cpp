a7a8e6844bb498f7ed6c6fc770ab3047245d4831
Ivo Batkovic <ivobatkovic@gmail.com>
2020-12-08 20:34:21 +0100

    2020 day5 solution

---
+++ 2020/day5/cpp/src/day5.cpp
@@ -0,0 +1,52 @@
+#include "day5.hpp"
+
+#include <algorithm>
+
+#include "utils.hpp"
+using day5::input_type;
+
+input_type day5::transform_input(std::vector<std::string> &input) {
+  input_type output;
+
+  for (auto &inp : input) {
+    int row[] = {0, 127};
+    int col[] = {0, 7};
+    for (auto &seat : inp) {
+      if (seat == 'B') {
+        row[0] = (row[0] + 1 + row[1]) / 2;
+      } else if (seat == 'F') {
+        row[1] = (row[0] + row[1]) / 2;
+      } else if (seat == 'R') {
+        col[0] = (col[0] + 1 + col[1]) / 2;
+      } else if (seat == 'L') {
+        col[1] = (col[0] + col[1]) / 2;
+      }
+    }
+    output.push_back(row[0] * 8 + col[0]);
+  }
+  return output;
+}
+
+input_type day5::read_input(std::string file_name) {
+  auto input = utils::read_file(file_name);
+  return transform_input(input);
+}
+
+// Implement part one solution
+std::string day5::solve_part1(input_type input_) {
+  return std::to_string(*std::max_element(input_.begin(), input_.end()));
+  return "Implement part one";
+}
+
+// Implement part two solution
+std::string day5::solve_part2(input_type input_) {
+  // Go through input and sort it
+  std::sort(input_.begin(), input_.end());
+
+  for (size_t i = 0; i < input_.size() - 1; i++) {
+    if (abs(input_[i] - input_[i + 1]) > 1) {
+      return std::to_string(input_[i] + 1);
+    }
+  }
+  return "Implement part two";
+}
---
+++ 2020/day5/cpp/src/test.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+
+#include <tuple>
+
+#include "day5.hpp"
+
+using std::string;
+
+class PartOne : public ::testing::TestWithParam<std::tuple<string, string>> {};
+
+TEST_P(PartOne, PartOneTest) {
+  auto param = GetParam();
+  auto file_name = string(SOURCE_DIR) + "/data/" + std::get<0>(param);
+  auto output = std::get<1>(param);
+  EXPECT_EQ(output, day5::solve_part1(day5::read_input(file_name)));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    PartOneTest, PartOne,
+    ::testing::Values(std::make_tuple("test_input0.txt", "357"),
+                      std::make_tuple("test_input1.txt", "567"),
+                      std::make_tuple("test_input2.txt", "119"),
+                      std::make_tuple("test_input3.txt", "820")));
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
