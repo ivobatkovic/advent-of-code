4a3d73d6a5acee617a8cded58bf66bb2cb7587c3
Ivo Batkovic <ivobatkovic@gmail.com>
2020-12-08 20:33:56 +0100

    2020 day2 cpp solution

---
+++ 2020/day2/cpp/src/day2.cpp
@@ -0,0 +1,59 @@
+#include "day2.hpp"
+
+#include <algorithm>
+
+#include "utils.hpp"
+
+using day2::input_type;
+
+input_type day2::transform_input(std::vector<std::string> &input) {
+  input_type output;
+
+  for (auto &inp : input) {
+    std::vector<std::string> split_input;
+    std::replace(inp.begin(), inp.end(), ':', ' ');
+    utils::split_string(inp, ' ', split_input);
+
+    auto count = split_input[0], letter = split_input[1], pw = split_input[2];
+
+    std::vector<std::string> split_count;
+    utils::split_string(count, '-', split_count);
+    int low = std::stoi(split_count[0]), high = std::stoi(split_count[1]);
+
+    output.push_back(std::make_tuple(low, high, letter[0], pw));
+  }
+
+  return output;
+}
+
+input_type day2::read_input(std::string file_name) {
+  auto input = utils::read_file(file_name);
+  return transform_input(input);
+}
+
+// Implement part one solution
+std::string day2::solve_part1(input_type input_) {
+  int n_pws(0);
+  for (auto &i : input_) {
+    auto low = std::get<0>(i), high = std::get<1>(i);
+    auto letter = std::get<2>(i);
+    auto pw = std::get<3>(i);
+
+    auto n_matches = std::count(pw.begin(), pw.end(), letter);
+    n_pws += (low <= n_matches && n_matches <= high) ? 1 : 0;
+  }
+  return std::to_string(n_pws);
+}
+
+// Implement part two solution
+std::string day2::solve_part2(input_type input_) {
+  int n_pws(0);
+  for (auto &i : input_) {
+    auto low = std::get<0>(i), high = std::get<1>(i);
+    auto letter = std::get<2>(i);
+    auto pw = std::get<3>(i);
+
+    n_pws += (pw[low - 1] == letter) ^ (pw[high - 1] == letter) ? 1 : 0;
+  }
+  return std::to_string(n_pws);
+}
---
+++ 2020/day2/cpp/src/test.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+
+#include <tuple>
+
+#include "day2.hpp"
+
+using std::string;
+
+class PartOne : public ::testing::TestWithParam<std::tuple<string, string>> {};
+
+TEST_P(PartOne, PartOneTest) {
+  auto param = GetParam();
+  auto file_name = string(SOURCE_DIR) + "/data/" + std::get<0>(param);
+  auto output = std::get<1>(param);
+  EXPECT_EQ(output, day2::solve_part1(day2::read_input(file_name)));
+}
+
+INSTANTIATE_TEST_SUITE_P(PartOneTest, PartOne,
+                         ::testing::Values(std::make_tuple("test_input0.txt",
+                                                           "2")));
+
+class PartTwo : public ::testing::TestWithParam<std::tuple<string, string>> {};
+
+TEST_P(PartTwo, PartTwoTest) {
+  auto param = GetParam();
+  auto file_name = string(SOURCE_DIR) + "/data/" + std::get<0>(param);
+  auto output = std::get<1>(param);
+  EXPECT_EQ(output, day2::solve_part2(day2::read_input(file_name)));
+}
+
+INSTANTIATE_TEST_SUITE_P(PartTwoTest, PartTwo,
+                         ::testing::Values(std::make_tuple("test_input0.txt",
+                                                           "1")));
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
