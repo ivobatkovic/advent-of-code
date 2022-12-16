88e906fb054e75312a567664f9edc57863a55a84
ivobatkovic <ivobatkovic@gmail.com>
2022-12-16 21:28:28 +0100

    removed unnecessary std::

--- year2022/day2/cpp/solutions.cpp
+++ year2022/day2/cpp/solutions.cpp
@@ -20,13 +20,13 @@ static input_type transform_input(const std::string &input_string) {
 
 std::string solve_part1(const std::string &input_string) {
     auto input = transform_input(input_string);
-    const std::unordered_map<std::string, std::size_t> rule_book{
+    const std::unordered_map<std::string, size_t> rule_book{
         {"A X", 1U + 3U}, {"A Y", 2U + 6U}, {"A Z", 3U + 0U},
         {"B X", 1U + 0U}, {"B Y", 2U + 3U}, {"B Z", 3U + 6U},
         {"C X", 1U + 6U}, {"C Y", 2U + 0U}, {"C Z", 3U + 3U},
     };
 
-    auto x = [&rule_book](std::size_t lhs, std::string rhs) {
+    auto x = [&rule_book](size_t lhs, std::string rhs) {
         return lhs + rule_book.at(rhs);
     };
     return std::to_string(std::accumulate(input.begin(), input.end(), 0U, x));
@@ -35,13 +35,13 @@ std::string solve_part1(const std::string &input_string) {
 std::string solve_part2(const std::string &input_string) {
     auto input = transform_input(input_string);
 
-    const std::unordered_map<std::string, std::size_t> rule_book{
+    const std::unordered_map<std::string, size_t> rule_book{
         {"A X", 3U + 0U}, {"A Y", 1U + 3U}, {"A Z", 2U + 6U},
         {"B X", 1U + 0U}, {"B Y", 2U + 3U}, {"B Z", 3U + 6U},
         {"C X", 2U + 0U}, {"C Y", 3U + 3U}, {"C Z", 1U + 6U},
     };
 
-    auto x = [&rule_book](std::size_t lhs, std::string rhs) {
+    auto x = [&rule_book](size_t lhs, std::string rhs) {
         return lhs + rule_book.at(rhs);
     };
     return std::to_string(std::accumulate(input.begin(), input.end(), 0U, x));
--- year2022/day4/cpp/solutions.cpp
+++ year2022/day4/cpp/solutions.cpp
@@ -13,15 +13,15 @@ namespace year2022 {
 
 namespace day4 {
 
-using input_type = std::vector<std::array<std::size_t, 4>>;
+using input_type = std::vector<std::array<size_t, 4>>;
 
 static input_type transform_input(const std::string &input_string) {
     input_type output{};
     for (auto &row : utils::split_string<std::string>(input_string, "\n")) {
-        std::array<std::size_t, 4U> nrs{};
-        std::size_t count{0U};
+        std::array<size_t, 4U> nrs{};
+        size_t count{0U};
         for (auto &x : utils::split_string<std::string>(row, ",")) {
-            auto numbers = utils::split_string<std::size_t>(x, "-");
+            auto numbers = utils::split_string<size_t>(x, "-");
             nrs.at(count++) = numbers.at(0U);
             nrs.at(count++) = numbers.at(1U);
         }
@@ -29,14 +29,15 @@ static input_type transform_input(const std::string &input_string) {
         output.push_back(
             (nrs[1] - nrs[0] > nrs[3] - nrs[2])
                 ? nrs
-                : std::array<std::size_t, 4U>{nrs[2], nrs[3], nrs[0], nrs[1]});
+                : std::array<size_t, 4U>{nrs[2], nrs[3], nrs[0], nrs[1]});
     }
     return output;
+    asda
 }
 
 std::string solve_part1(const std::string &input_string) {
     auto input = transform_input(input_string);
-    auto intersection = [](std::size_t lhs, std::array<std::size_t, 4U> x) {
+    auto intersection = [](size_t lhs, std::array<size_t, 4U> x) {
         auto &[l1, r1, l2, r2] = x;
         return (l1 <= l2 && r1 >= r2) ? lhs + 1U : lhs;
     };
@@ -46,7 +47,7 @@ std::string solve_part1(const std::string &input_string) {
 
 std::string solve_part2(const std::string &input_string) {
     auto input = transform_input(input_string);
-    auto intersection = [](std::size_t lhs, std::array<std::size_t, 4U> x) {
+    auto intersection = [](size_t lhs, std::array<size_t, 4U> x) {
         auto &[l1, r1, l2, r2] = x;
         return ((l1 <= l2 && r1 >= l2) || (l1 <= r2 && r1 >= r2)) ? lhs + 1U
                                                                   : lhs;
