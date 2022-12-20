65465576c2f174fb31c0306885720f6f7fae84ad
ivobatkovic <ivobatkovic@gmail.com>
2022-12-20 07:55:05 +0100

    c++/python solutions year2022 day 9

---
+++ year2022/day9/cpp/solutions.hpp
@@ -0,0 +1,18 @@
+#ifndef _YEAR2022_DAY9_CPP_H_
+#define _YEAR2022_DAY9_CPP_H_
+
+#include <string>
+
+namespace year2022 {
+
+namespace day9 {
+
+std::string solve_part1(const std::string &input_string);
+
+std::string solve_part2(const std::string &input_string);
+
+}  // namespace day9
+
+}  // namespace year2022
+
+#endif
