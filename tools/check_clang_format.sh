0c7dc76e92f05a1839007d43ecfa5856ea77a840
ivobatkovic <ivobatkovic@gmail.com>
2025-12-23 11:42:09 +0100

    Fix CI jobs

--- tools/check_clang_format.sh
+++ tools/check_clang_format.sh
@@ -3,18 +3,21 @@
 exit_code=0
 
 # Append where to start looking from
-if [ $# -eq 0 ]; then start="."
-    else start=$1
+if [ $# -eq 0 ]; then
+    folders=$(git ls-files '*.cpp' '*.h' | awk -F/ '{print $1}' | sort -u)
+else 
+    folders=$1
 fi
-source_files=$(find $start -type f \( -iname \*.cpp -o -iname \*.c \))
-header_files=$(find $start -type f \( -iname \*.hpp -o -iname \*.h \))
-for f in $source_files $header_files; do
-    output=$(clang-format -sort-includes -output-replacements-xml \
-           $f | grep -c "<replacement ")
-    if [ $output -ne "0" ]; then
-        echo File $f needs reformatting
-        exit_code=1
-    fi
+
+for folder in $folders; do
+    source_files=$(find "$folder" -type f \( -iname "*.cpp" -o -iname "*.hpp" \))
+    for f in $source_files; do
+        output=$(clang-format -sort-includes -output-replacements-xml $f | grep -c "<replacement ")
+        if [ $output -ne "0" ]; then
+            echo File $f needs reformatting
+            exit_code=1
+        fi
+    done
 done
 echo Done checking
 exit $exit_code
--- utils/utils.cpp
+++ utils/utils.cpp
@@ -3,8 +3,6 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
-#include <string>
-#include <vector>
 
 namespace utils {
 
