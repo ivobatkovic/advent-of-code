408f6105b67ebb81ea1c61b5be1f6ba029a6edaf
ivobatkovic <ivobatkovic@gmail.com>
2026-01-05 08:08:20 +0100

    2015-03

---
+++ year2015/day3/cpp/test.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+
+#include <string>
+#include <tuple>
+
+#include "utils/utils.hpp"
+#include "year2015/day3/cpp/solutions.hpp"
+
+using std::string;
+
+namespace year2015 {
+
+namespace day3 {
+
+class TestYear2015Day3PartOne
+    : public ::testing::TestWithParam<std::tuple<string, string>> {};
+
+TEST_P(TestYear2015Day3PartOne, PartOne) {
+    auto param = GetParam();
+    auto file_name = "year2015/day3/data/" + std::get<0>(param);
+    auto output = std::get<1>(param);
+    EXPECT_EQ(output, day3::solve_part1(utils::read_file_to_string(file_name)));
+}
+
+INSTANTIATE_TEST_SUITE_P(PartOne, TestYear2015Day3PartOne,
+                         ::testing::Values(std::make_tuple("test_input0.txt",
+                                                           "2")));
+
+class TestYear2015Day3PartTwo
+    : public ::testing::TestWithParam<std::tuple<string, string>> {};
+
+TEST_P(TestYear2015Day3PartTwo, PartTwo) {
+    auto param = GetParam();
+    auto file_name = "year2015/day3/data/" + std::get<0>(param);
+    auto output = std::get<1>(param);
+    EXPECT_EQ(output, day3::solve_part2(utils::read_file_to_string(file_name)));
+}
+
+INSTANTIATE_TEST_SUITE_P(PartTwo, TestYear2015Day3PartTwo,
+                         ::testing::Values(std::make_tuple("test_input0.txt",
+                                                           "11")));
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+}  // namespace day3
+
+}  // namespace year2015
