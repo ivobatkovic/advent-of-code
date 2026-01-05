f647ce464ebd66a01de8c41d1c5248ece2d8b630
ivobatkovic <ivobatkovic@gmail.com>
2026-01-05 19:28:43 +0100

    2021-03

---
+++ year2021/day3/cpp/solutions.hpp
@@ -0,0 +1,18 @@
+#ifndef _YEAR2021_DAY3_CPP_H_
+#define _YEAR2021_DAY3_CPP_H_
+
+#include <string>
+
+namespace year2021 {
+
+namespace day3 {
+
+std::string solve_part1(const std::string &input_string);
+
+std::string solve_part2(const std::string &input_string);
+
+}  // namespace day3
+
+}  // namespace year2021
+
+#endif
